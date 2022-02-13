4c54280d6061f1493f77b64abaea600b4fd10b1e
Noel Luna <noelluna@Noels-MacBook-Pro-2.local>
2022-02-13 15:17:38 +0000

    Final Push

---
+++ build/precache-manifest.da611dee105f7b570c2156d6e512f550.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013801b5c6c577149de6297b95c3eb14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "534f01458dcb812e5e6c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "efcc1f602f8969b8146c",
+    "url": "/static/js/2.3eaddf6d.chunk.js"
+  },
+  {
+    "revision": "767d7078eb986071a358",
+    "url": "/static/js/3.b1c1d167.chunk.js"
+  },
+  {
+    "revision": "534f01458dcb812e5e6c",
+    "url": "/static/js/main.f3650dbd.chunk.js"
+  },
+  {
+    "revision": "7e3cad9ed6308eb5ead4",
+    "url": "/static/js/runtime~main.cbc17e78.js"
+  },
+  {
+    "revision": "2c8c67e1ee869a3597a874ef01a0b9f2",
+    "url": "/static/media/Luna_Noel.2c8c67e1.pdf"
+  },
+  {
+    "revision": "3d1e0fedc168099c91bfe99aaaeccdec",
+    "url": "/static/media/img-1.3d1e0fed.jpg"
+  },
+  {
+    "revision": "dcf64fa997eeecfff98e1295be3e176b",
+    "url": "/static/media/img-modal1.dcf64fa9.JPG"
+  }
+]);
